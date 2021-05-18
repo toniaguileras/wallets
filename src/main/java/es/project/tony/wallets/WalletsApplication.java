4028d0257b7e43d0ada90d6d7832913de143bc55
Tony Aguilera <toni.aguilera@cabsa.es>
2021-05-18 11:38:07 +0200

    First commit, welcome to wallets

---
+++ src/main/java/es/project/tony/wallets/WalletsApplication.java
@@ -0,0 +1,13 @@
+package es.project.tony.wallets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/project/tony/wallets/WalletsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.project.tony.wallets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
