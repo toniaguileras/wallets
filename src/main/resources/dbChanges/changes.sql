0478144f23b74e7658db92a53467031fe6cfe100
Tony Aguilera <toni.aguilera@cabsa.es>
2021-05-25 07:25:00 +0200

    Adds inserts necessaries to start project

--- src/main/resources/dbChanges/changes.sql
+++ src/main/resources/dbChanges/changes.sql
@@ -1,7 +1,7 @@
 insert into user (email, name, role, surname, username)
 values ('toniaguileras@gmail.com', 'Toni', 'ADMIN', 'Aguilera', 'toni_aguilera');
 insert into user (email, name, role, surname, username)
-values ('alexdiaz@gmail.com', 'Alex', 'ADMIN', 'Diaz', 'alex_diaz');
+values ('alexdiaz@gmail.com', 'Alex', 'USER', 'Diaz', 'alex_diaz');
 
 insert into user (email, name, role, surname, username)
 values ('angelramirez@gmail.com', 'Angel', 'ADMIN', 'Ramirez', 'angel_ramirez');
@@ -12,3 +12,7 @@ insert into wallet (amount, name, user_id)
 values (2000.00, 'Cartera de Toni 2', 1);
 insert into wallet (amount, name, user_id)
 values (1500.00, 'Cartera de Alex 1', 2);
+
+insert into user_wallets values (1,1);
+insert into user_wallets values (1,2);
+insert into user_wallets values (1,3);
